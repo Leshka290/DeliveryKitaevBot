754de37c3c67926033581320c9361d3fc67aa140
leshka <lleesshhkkaa00@gmail.com>
2023-09-08 08:58:31 +0300

    First commit

---
+++ src/main/java/com/tgproject/deliverykitaevbot/DeliveryKitaevBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgproject.deliverykitaevbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryKitaevBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryKitaevBotApplication.class, args);
+    }
+
+}
